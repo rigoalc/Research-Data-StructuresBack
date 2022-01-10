2c92821f59877918b3b43166276aba5bc74b0230
Rigo Alcover <rodigomartinalcover@gmail.com>
2022-01-10 09:23:31 -0500

    Create Research Data StructuresBack.py

---
+++ Research Data StructuresBack.py
@@ -0,0 +1,21 @@
+#Research Data StructuresBack
+
+
+#Research different types of Data Structures. 
+# These Data Structures are used in many different programming languages, 
+# so you don't need to find Python specific information, although it is suggested.
+
+#Requirements for credit (all must be satisfied):
+#Header with your name, date, CRN, and class name
+#Research the following Data Structures:
+#2D Array
+#Linked List
+#Stack
+#Queue
+#Dictionary / Hash Map
+#Set
+#For each Data Structure have 1-2 paragraphs summarizing:
+#Define what it is (how is it different from the others)
+#1 or more use cases (why would you use this data structure instead of the others)
+#Each paragraph should be 3-5 sentences
+
