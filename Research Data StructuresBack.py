0b0c7f5ccb0c0f45305f5f9b91a8f452a32dca6b
Rigo Alcover <rodigomartinalcover@gmail.com>
2022-01-14 11:19:23 -0500

    Update Research Data StructuresBack.py

--- Research Data StructuresBack.py
+++ Research Data StructuresBack.py
@@ -19,4 +19,42 @@
 #1 or more use cases (why would you use this data structure instead of the others)
 #Each paragraph should be 3-5 sentences
 
-#Array is a data structure that source elements of the same data in a secuential order. Two dimentional arrays 
+
+#2D Array
+
+#An array is a data structure that sources elements of the same data in sequential order. 
+#Two-dimensional is also a data structure but in this case stores two indices instead of one. 
+#An array of an array is represented in a table with rows and columns of data.
+#It can be used for a table score in a tournament.
+
+#Linked List
+#The difference between a list and liked list is in how it stores the data in the memory. A linked list uses a part of the element as a reference, and a list uses a contiguous memory block.
+#Advantages of this data structure are: the memory is allocated at run-time, insertion and deletion operations take less time. It can be used for example to implement queues.
+
+#Stack
+
+#Stack is a linear data structure with an order of how the operations are performed.
+#Stack uses the order Last-In/First-Out (LIFO) manner to store items.
+#The main difference from an array is that stacks can store elements of different types.
+#Some examples of this data structure can be found in the Back/Forward stacks on browsers or, Undo/Redo stacks in Excel or Word.
+
+#Queue
+
+#Queue is also is a linear data structure similar to stack; with an order of how the operations are performed.
+#Queue uses the order First-In/Last-Out (FILO) manner to store items.
+#As a difference with stack(all insertions are made at the same end), a queue is exactly the opposite because it is open at both ends; one for insert and the other to remove data.
+#It can be used for example for a wait in DMV or customer service, multimedia playlist, or Starbucks orders. 
+
+#Dictionary / Hash Map
+
+#A dictionary is an unordered and mutable Python data structure that stores mappings of unique keys to values.
+#Dictionaries use keys for indexing. They are an unordered sequence of items. The keys are immutable.
+#An example could be just a dictionary.
+
+#Set
+
+#Sets are used to store multiple items in a single variable.
+#A set is a collection of data unordered, unchangeable, and unindexed.
+#It cannot have multiple occurrences of the same element.
+#An example of the use of sets is to remove duplicate elements from a sequence.
+ 
